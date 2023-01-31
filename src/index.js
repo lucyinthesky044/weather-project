a7627d63e3bd00cae37f6d5be078153bc6b24176
lucyinthesky044 <121825785+lucyinthesky044@users.noreply.github.com>
2023-01-31 11:28:01 -0500

    debagged the index.js file

--- src/index.js
+++ src/index.js
@@ -46,6 +46,71 @@ function formatDate(timestamp) {
   return `Today is ${day} ${date} ${month} ${year}, ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return days[day];
+}
+
+function showForecast(response) {
+  let forecast = response.data.dily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row align-items-center">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-4" id="weather-forecast-date">
+        ${formatDay(forecastDay.dt)}
+        <div id="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max">${Math.round(
+            forecastDay.temp.max
+          )}</span>
+          <span class="weather-forecast-temperature-min">${Math.round(
+            forecastDay.temp.min
+          )}</span>
+        </div>
+      </div>
+      <div class="col-4">
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="80"
+        />
+      </div>
+      <div class="col-4" id="weather-forecast-description">
+        Partly Cloudy
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let key = "a5acb752426cd8188485c35694980e3a";
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&units=metric`;
+  axios.get(url).then(showForecast);
+}
+
 function showWeather(response) {
   celsiusTemp = response.data.main.temp;
 
@@ -86,26 +151,38 @@ function showWeather(response) {
   getForecast(response.data.coord);
 }
 
-function getForecast(coordinates) {
-  let key = "72bb9dab46b9ec3d65f423c63f27a9b8";
-  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&units=metric`;
-  axios.get(url).then(showForecast);
-}
-
 function searchCity(city) {
-  let key = "72bb9dab46b9ec3d65f423c63f27a9b8";
+  let key = "a5acb752426cd8188485c35694980e3a";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
   axios.get(url).then(showWeather);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#search-bar").value.trim();
-  searchCity(city);
+  let city = document.querySelector("#search-bar").trim();
+  searchCity(city.value);
+}
+
+function showFahrenheitTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#main-temp");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+}
+
+function showCelsiusTemp(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  let temperatureElement = document.querySelector("#main-temp");
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
 }
 
 function searchLocation(position) {
-  let key = "72bb9dab46b9ec3d65f423c63f27a9b8";
+  let key = "a5acb752426cd8188485c35694980e3a";
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${key}&units=metric`;
   axios.get(url).then(showWeather);
 }
@@ -115,64 +192,13 @@ function getCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-function showFahrenheitTemp(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#main-temp");
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
-}
-
-function showCelsiusTemp(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#main-temp");
-  fahrenheitLink.classList.remove("active");
-  celsiusLink.classList.add("active");
-  temperatureElement.innerHTML = Math.round(celsiusTemp);
-}
-
-function showForecast(response) {
-  console.log(response.data.daily);
-  let forecastElement = document.querySelector("#forecast");
-  let days = ["Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-
-  let forecastHTML = `<div class="row align-items-center">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-      <div class="col-4" id="weather-forecast-date">
-        ${day}
-        <div id="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max">-1° </span>
-          <span class="weather-forecast-temperature-min">1°</span>
-        </div>
-      </div>
-      <div class="col-4">
-        <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
-          alt=""
-          width="80"
-        />
-      </div>
-      <div class="col-4" id="weather-forecast-description">
-        Partly Cloudy
-      </div>
-  `;
-  });
-
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
-}
+let celsiusTemp = null;
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
 let searchBotton = document.querySelector("#search-botton");
 searchBotton.addEventListener("click", handleSubmit);
-
-let celsiusTemp = null;
 
 let locationBotton = document.querySelector("#location-botton");
 locationBotton.addEventListener("click", getCurrentLocation);
